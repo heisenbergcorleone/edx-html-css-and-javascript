e103bbb8f272d623b08fcd1e6cb088cc04c3a52e
Karan <ksinghdhir1@gmail.com>
2016-12-30 16:44:00 +0530

    handling js exceptions

---
+++ Season04/handlingjsexceptions.js
@@ -0,0 +1,13 @@
+function correctMethodName() {
+	return "Output Message";
+}
+
+
+try {
+var result = wrongMethodName();
+
+document.writeln(result);
+}
+catch (error) {
+	document.writeln('<h3>Error:&nbsp;</h3><p>' + error + "</p>");
+}
